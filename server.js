2d72fba09ef4168e6bedf3ae3381b2b81e9db99c
Michael Resch <102878929+mresch03@users.noreply.github.com>
2022-05-03 21:04:33 +0200

    change response

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) });
+app.get('/', (req, res) => { res.send('its working') });
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
