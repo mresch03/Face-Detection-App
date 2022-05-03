7435017b15f9a720caee7ccd52206d42702a3e8c
Michael Resch <102878929+mresch03@users.noreply.github.com>
2022-05-03 20:25:17 +0200

    FDA-files
    Face Detection App
    server
    knex
    postgresql

---
+++ controllers/image.js
@@ -0,0 +1,35 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'c221f6a986824019ad58be40b01aab2a'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with api'))
+}
+
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users')
+		.where('id', '=', id)
+  	.increment('entries', 1)
+  	.returning('entries')	
+  	.then(entries => {
+  		res.json(entries[0].entries);
+  	})
+  	.catch(err => {
+  		res.status(400).json('unable to get entries')
+  	})
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
+
