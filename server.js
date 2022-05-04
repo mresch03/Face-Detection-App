d9470a375626ec14690503e45f8a536c9c592f3b
Michael Resch <102878929+mresch03@users.noreply.github.com>
2022-05-04 07:57:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    port : 5432,
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    ssl: true, 
   }
 });
 
