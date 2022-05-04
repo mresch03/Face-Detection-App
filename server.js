1cacfe563b8a5e212b392d50998657a2f13c0b3d
Michael Resch <102878929+mresch03@users.noreply.github.com>
2022-05-04 08:01:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : process.env.DATABASE_URL,
     ssl: true, 
   }
 });
